edb12f339691ee68d770eae82301a0be6dfa16a2
99sparshm <99.sparsh@gmail.com>
2020-11-03 14:22:26 +0530

    removed redundant logs

--- client/src/Pages/Admin.js
+++ client/src/Pages/Admin.js
@@ -4,217 +4,247 @@ import "./que.css";
 import Navbar from "./Navbar";
 
 class Admin extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      addques: false,
-      addhint: false,
-      qno: 0,
-      body: "",
-      points: "",
-      answer: "",
-      qid: "",
-      hint: "",
-      vis: 0,
-      message: ""
-    };
-    this.getQues = this.getQues.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            addques: false,
+            addhint: false,
+            qno: 0,
+            body: "",
+            points: "",
+            answer: "",
+            qid: "",
+            hint: "",
+            vis: 0,
+            message: "",
+        };
+        this.getQues = this.getQues.bind(this);
+    }
 
-  submitQues() {
-    this.setState({ isQues: false });
-    fetch(`/api/admin/addquestion`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        qno: this.state.qno,
-        body: this.state.body,
-        answer: this.state.answer.toLowerCase().replace(/\s/g, ""),
-        points: this.state.points
-      })
-    })
-      .then(resp => {
-        return resp.json();
-      })
-      .then(data => {
-        if (!data.success && String(data.msg) === "Login First!")
-          this.props.history.push("/login");
-        else if (!data.success && String(data.msg) === "Unauthorized access")
-          this.props.history.push("/home");
-        else {
-          this.setState({ message: data["msg"], addques: false });
-          this.props.history.push("/admin");
-          this.setState({ qno: "", body: "", answer: "", points: "" });
-        }
-      })
-      .catch(err => console.log(err));
-  }
+    submitQues() {
+        this.setState({ isQues: false });
+        fetch(`/api/admin/addquestion`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                qno: this.state.qno,
+                body: this.state.body,
+                answer: this.state.answer.toLowerCase().replace(/\s/g, ""),
+                points: this.state.points,
+            }),
+        })
+            .then((resp) => {
+                return resp.json();
+            })
+            .then((data) => {
+                if (!data.success && String(data.msg) === "Login First!")
+                    this.props.history.push("/login");
+                else if (
+                    !data.success &&
+                    String(data.msg) === "Unauthorized access"
+                )
+                    this.props.history.push("/home");
+                else {
+                    this.setState({ message: data["msg"], addques: false });
+                    this.props.history.push("/admin");
+                    this.setState({
+                        qno: "",
+                        body: "",
+                        answer: "",
+                        points: "",
+                    });
+                }
+            })
+            .catch((err) => console.log(err));
+    }
 
-  submitHint() {
-    this.setState({ isHint: false });
-    fetch(`/api/admin/addhint`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        qid: parseInt(this.state.qno),
-        body: this.state.hint,
-        visibility: parseInt(this.state.vis)
-      })
-    })
-      .then(resp => {
-        return resp.json();
-      })
-      .then(data => {
-        if (!data.success && String(data.msg) === "Login First!")
-          this.props.history.push("/login");
-        else if (!data.success && String(data.msg) === "Unauthorized access")
-          this.props.history.push("/home");
-        else {
-          console.log(data);
-          this.setState({ message: data["msg"], addques: false });
-          this.props.history.push("/admin");
-          this.setState({ qno: "" });
-        }
-      })
-      .catch(err => console.log(err));
-  }
+    submitHint() {
+        this.setState({ isHint: false });
+        fetch(`/api/admin/addhint`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                qid: parseInt(this.state.qno),
+                body: this.state.hint,
+                visibility: parseInt(this.state.vis),
+            }),
+        })
+            .then((resp) => {
+                return resp.json();
+            })
+            .then((data) => {
+                if (!data.success && String(data.msg) === "Login First!")
+                    this.props.history.push("/login");
+                else if (
+                    !data.success &&
+                    String(data.msg) === "Unauthorized access"
+                )
+                    this.props.history.push("/home");
+                else {
+                    this.setState({ message: data["msg"], addques: false });
+                    this.props.history.push("/admin");
+                    this.setState({ qno: "" });
+                }
+            })
+            .catch((err) => console.log(err));
+    }
 
-  addQuestion() {
-    return (
-      <div>
-        <div>Question Number: {this.state.qno}</div>
-        <div>Question Body:</div>
-        <div>
-          <input
-            type="text"
-            placeholder="Question Body:"
-            value={this.state.body}
-            onChange={event => this.setState({ body: event.target.value })}
-          />
-        </div>
-        <div>Points:</div>
-        <div>
-          <input
-            type="text"
-            placeholder="Points:"
-            value={this.state.points}
-            onChange={event => this.setState({ points: event.target.value })}
-          />
-        </div>
-        <div>Answer:</div>
-        <div>
-          <input
-            type="text"
-            placeholder="Answer:"
-            value={this.state.answer}
-            onChange={event => this.setState({ answer: event.target.value })}
-          />
-        </div>
-        <div>
-          <button onClick={() => this.submitQues()}>Submit</button>
-        </div>
-      </div>
-    );
-  }
-
-  addHint() {
-    return (
-      <div>
-        <div>Question ID: {this.state.qno}</div>
-        <div>Hint:</div>
-        <div>
-          <input
-            type="text"
-            placeholder="Hints"
-            value={this.state.hint}
-            onChange={event => this.setState({ hint: event.target.value })}
-          />
-        </div>
-        <div>Visibility:</div>
-        <div>
-          <input
-            type="text"
-            placeholder="Visibility"
-            value={this.state.vis}
-            onChange={event => this.setState({ vis: event.target.value })}
-          />
-        </div>
-        <div>
-          <button onClick={() => this.submitHint()}>Submit</button>
-        </div>
-      </div>
-    );
-  }
-
-  getQues(cell) {
-    this.setState({ qno: cell });
-  }
-
-  componentDidMount() {
-    this.setState({ qno: "" });
-    fetch("/api/home", {
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-      .then(resp => {
-        return resp.json();
-      })
-      .then(data => {
-        if (!data.success && String(data.msg) === "Login First!")
-          this.props.history.push("/login");
-        else if (!data.success && String(data.msg) === "Unauthorized access")
-          this.props.history.push("/home");
-        else {
-          console.log(data);
-          if (data["msg"]["access"] !== 20) {
-            this.props.history.push("/");
-          }
-        }
-      })
-      .catch(err => console.log(err));
-  }
-
-  render() {
-    return (
-      <div className="master">
-        <Navbar />
-        {this.state.message}
-
-        {this.state.qno === "" ? (
-          <div>
-            <Maze fetchQues={this.getQues} />
-          </div>
-        ) : (
+    addQuestion() {
+        return (
             <div>
-              {this.state.qno}
-              {!(this.state.addques || this.state.addhint) ? (
+                <div>Question Number: {this.state.qno}</div>
+                <div>Question Body:</div>
                 <div>
-                  <button onClick={() => this.setState({ addques: true })}>
-                    Add Question
-                </button>
-                  <button onClick={() => this.setState({ addhint: true })}>
-                    Add Hints
-                </button>
+                    <input
+                        type="text"
+                        placeholder="Question Body:"
+                        value={this.state.body}
+                        onChange={(event) =>
+                            this.setState({ body: event.target.value })
+                        }
+                    />
                 </div>
-              ) : (
-                  <div>
-                    {this.state.addques ? (
-                      <div>{this.addQuestion()}</div>
-                    ) : (
-                        <div>{this.addHint()}</div>
-                      )}
-                  </div>
+                <div>Points:</div>
+                <div>
+                    <input
+                        type="text"
+                        placeholder="Points:"
+                        value={this.state.points}
+                        onChange={(event) =>
+                            this.setState({ points: event.target.value })
+                        }
+                    />
+                </div>
+                <div>Answer:</div>
+                <div>
+                    <input
+                        type="text"
+                        placeholder="Answer:"
+                        value={this.state.answer}
+                        onChange={(event) =>
+                            this.setState({ answer: event.target.value })
+                        }
+                    />
+                </div>
+                <div>
+                    <button onClick={() => this.submitQues()}>Submit</button>
+                </div>
+            </div>
+        );
+    }
+
+    addHint() {
+        return (
+            <div>
+                <div>Question ID: {this.state.qno}</div>
+                <div>Hint:</div>
+                <div>
+                    <input
+                        type="text"
+                        placeholder="Hints"
+                        value={this.state.hint}
+                        onChange={(event) =>
+                            this.setState({ hint: event.target.value })
+                        }
+                    />
+                </div>
+                <div>Visibility:</div>
+                <div>
+                    <input
+                        type="text"
+                        placeholder="Visibility"
+                        value={this.state.vis}
+                        onChange={(event) =>
+                            this.setState({ vis: event.target.value })
+                        }
+                    />
+                </div>
+                <div>
+                    <button onClick={() => this.submitHint()}>Submit</button>
+                </div>
+            </div>
+        );
+    }
+
+    getQues(cell) {
+        this.setState({ qno: cell });
+    }
+
+    componentDidMount() {
+        this.setState({ qno: "" });
+        fetch("/api/home", {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((resp) => {
+                return resp.json();
+            })
+            .then((data) => {
+                if (!data.success && String(data.msg) === "Login First!")
+                    this.props.history.push("/login");
+                else if (
+                    !data.success &&
+                    String(data.msg) === "Unauthorized access"
+                )
+                    this.props.history.push("/home");
+                else {
+                    if (data["msg"]["access"] !== 20) {
+                        this.props.history.push("/");
+                    }
+                }
+            })
+            .catch((err) => console.log(err));
+    }
+
+    render() {
+        return (
+            <div className="master">
+                <Navbar />
+                {this.state.message}
+
+                {this.state.qno === "" ? (
+                    <div>
+                        <Maze fetchQues={this.getQues} />
+                    </div>
+                ) : (
+                    <div>
+                        {this.state.qno}
+                        {!(this.state.addques || this.state.addhint) ? (
+                            <div>
+                                <button
+                                    onClick={() =>
+                                        this.setState({ addques: true })
+                                    }
+                                >
+                                    Add Question
+                                </button>
+                                <button
+                                    onClick={() =>
+                                        this.setState({ addhint: true })
+                                    }
+                                >
+                                    Add Hints
+                                </button>
+                            </div>
+                        ) : (
+                            <div>
+                                {this.state.addques ? (
+                                    <div>{this.addQuestion()}</div>
+                                ) : (
+                                    <div>{this.addHint()}</div>
+                                )}
+                            </div>
+                        )}
+                    </div>
                 )}
             </div>
-          )}
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default Admin;
--- client/src/Pages/Play.js
+++ client/src/Pages/Play.js
@@ -7,185 +7,192 @@ import Navbar from "./Navbar";
 import "./que.css";
 
 class Play extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      qno: 0,
-      alert: "",
-      data: {
-        body: "What is 1+1?",
-        hints: [
-          {
-            body: "addition"
-          },
-          {
-            body: "please add"
-          }
-        ]
-      },
-      loaded: false,
-      answer: ""
-    };
-    this.updateState = this.updateState.bind(this);
-  }
-
-  updateState(cell) {
-    this.setState({ loaded: false });
-    if (cell === 0) {
-      this.setState({ alert: "Question not Available" });
-      return;
-    }
-    if (cell === -1) {
-      this.setState({ alert: "Already Solved" });
-      return;
+    constructor(props) {
+        super(props);
+        this.state = {
+            qno: 0,
+            alert: "",
+            data: {
+                body: "What is 1+1?",
+                hints: [
+                    {
+                        body: "addition",
+                    },
+                    {
+                        body: "please add",
+                    },
+                ],
+            },
+            loaded: false,
+            answer: "",
+        };
+        this.updateState = this.updateState.bind(this);
     }
 
-    fetch(`/api/play/${cell}`, {
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-      .then(resp => {
-        return resp.json();
-      })
-      .then(datajson => {
-        if (!datajson.success) {
-          this.setState({
-            alert: "You Need to solve Previous Questions First"
-          });
-          return;
+    updateState(cell) {
+        this.setState({ loaded: false });
+        if (cell === 0) {
+            this.setState({ alert: "Question not Available" });
+            return;
         }
-        this.setState({
-          data: datajson.data,
-          loaded: true,
-          qno: cell,
-          alert: ""
-        });
-      })
-      .catch(err => console.log(err));
-  }
-
-  componentDidMount() {
-    fetch(`/api/play/rank`, {
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-      .then(resp => {
-        return resp.json();
-      })
-      .then(datajson => {
-        this.setState(
-          {
-            data: datajson.data
-          },
-          () => {
-            if (!datajson.success) {
-              this.props.history.push("/login");
-              this.setState({ loaded: true });
-            }
-          }
-        );
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
-
-  submit() {
-    fetch(`/api/play/submit?id=${this.state.qno}`, {
-      method: "POST",
-      body: JSON.stringify({
-        answer: this.state.answer.toLowerCase().replace(/\s/g, "")
-      }),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-      .then(resp => {
-        return resp.json();
-      })
-      .then(data => {
-        if (!data.success && String(data.msg) === "Login First!")
-          this.props.history.push("/login");
-        else if (!data.success && String(data.msg) === "Unauthorized access")
-          this.props.history.push("/home");
-        else {
-          console.log(data);
-          this.setState({ alert: data.msg });
-          if (data.msg === "Solved") {
-            this.setState({ loaded: false });
-            this.props.history.push("/home");
-          }
+        if (cell === -1) {
+            this.setState({ alert: "Already Solved" });
+            return;
         }
-      })
-      .catch(err => console.log(err));
 
-    this.setState({ answer: "" });
-  }
+        fetch(`/api/play/${cell}`, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((resp) => {
+                return resp.json();
+            })
+            .then((datajson) => {
+                if (!datajson.success) {
+                    this.setState({
+                        alert: "You Need to solve Previous Questions First",
+                    });
+                    return;
+                }
+                this.setState({
+                    data: datajson.data,
+                    loaded: true,
+                    qno: cell,
+                    alert: "",
+                });
+            })
+            .catch((err) => console.log(err));
+    }
 
-  render() {
-    return (
-      <>
-        <Navbar />
-        <div className="master">
-          <Popup />
-          {this.state.loaded ? (
-            <div className="que">
-              <Maze fetchQues={this.updateState} />
-              <div className="col2">
-                <div className="question">
-                  <h2>Question</h2>
-                  {this.state.qno}
-                  {this.state.data.body}
-                </div>
-                {this.state.data.hints.length !== 0 ? (
-                  <div className="hints">
-                    <h2>Hints</h2>
-                    {this.state.data.hints.map(function (value, i) {
-                      return (
-                        <div key={i}>
-                          {value.body}
-                          <br />
-                        </div>
-                      );
-                    })}
-                  </div>
-                ) : (
-                    ""
-                  )}
-                <div className="Input answer">
-                  <input
-                    className="textInput"
-                    type="text"
-                    placeholder="Answer..."
-                    value={this.state.answer}
-                    onChange={event =>
-                      this.setState({ answer: event.target.value })
+    componentDidMount() {
+        fetch(`/api/play/rank`, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((resp) => {
+                return resp.json();
+            })
+            .then((datajson) => {
+                this.setState(
+                    {
+                        data: datajson.data,
+                    },
+                    () => {
+                        if (!datajson.success) {
+                            this.props.history.push("/login");
+                            this.setState({ loaded: true });
+                        }
                     }
-                  ></input>
+                );
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+    submit() {
+        fetch(`/api/play/submit?id=${this.state.qno}`, {
+            method: "POST",
+            body: JSON.stringify({
+                answer: this.state.answer.toLowerCase().replace(/\s/g, ""),
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((resp) => {
+                return resp.json();
+            })
+            .then((data) => {
+                if (!data.success && String(data.msg) === "Login First!")
+                    this.props.history.push("/login");
+                else if (
+                    !data.success &&
+                    String(data.msg) === "Unauthorized access"
+                )
+                    this.props.history.push("/home");
+                else {
+                    this.setState({ alert: data.msg });
+                    if (data.msg === "Solved") {
+                        this.setState({ loaded: false });
+                        this.props.history.push("/home");
+                    }
+                }
+            })
+            .catch((err) => console.log(err));
+
+        this.setState({ answer: "" });
+    }
+
+    render() {
+        return (
+            <>
+                <Navbar />
+                <div className="master">
+                    <Popup />
+                    {this.state.loaded ? (
+                        <div className="que">
+                            <Maze fetchQues={this.updateState} />
+                            <div className="col2">
+                                <div className="question">
+                                    <h2>Question</h2>
+                                    {this.state.qno}
+                                    {this.state.data.body}
+                                </div>
+                                {this.state.data.hints.length !== 0 ? (
+                                    <div className="hints">
+                                        <h2>Hints</h2>
+                                        {this.state.data.hints.map(function (
+                                            value,
+                                            i
+                                        ) {
+                                            return (
+                                                <div key={i}>
+                                                    {value.body}
+                                                    <br />
+                                                </div>
+                                            );
+                                        })}
+                                    </div>
+                                ) : (
+                                    ""
+                                )}
+                                <div className="Input answer">
+                                    <input
+                                        className="textInput"
+                                        type="text"
+                                        placeholder="Answer..."
+                                        value={this.state.answer}
+                                        onChange={(event) =>
+                                            this.setState({
+                                                answer: event.target.value,
+                                            })
+                                        }
+                                    ></input>
+                                </div>
+                                <div>
+                                    <div>{this.state.alert}</div>
+                                    <button
+                                        className="button button-block submit submitplay"
+                                        onClick={() => this.submit()}
+                                    >
+                                        Submit
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    ) : (
+                        <div className="que2">
+                            <Maze fetchQues={this.updateState} />
+                            <div className="alert">{this.state.alert}</div>
+                        </div>
+                    )}
                 </div>
-                <div>
-                  <div>{this.state.alert}</div>
-                  <button
-                    className="button button-block submit submitplay"
-                    onClick={() => this.submit()}
-                  >
-                    Submit
-                  </button>
-                </div>
-              </div>
-            </div>
-          ) : (
-              <div className="que2">
-                <Maze fetchQues={this.updateState} />
-                <div className="alert">{this.state.alert}</div>
-              </div>
-            )}
-        </div>
-      </>
-    );
-  }
+            </>
+        );
+    }
 }
 
 export default withRouter(Play);
